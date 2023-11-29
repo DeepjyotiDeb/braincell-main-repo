f9ab5da7a8363f17a06d2c585f75cb37a8a8f12f
Deepjyoti Deb <55659244+DeepjyotiDeb@users.noreply.github.com>
2023-11-29 13:36:29 +0530

    feat: updated read me

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # braincell-main-repo
 
 Frontend for the Baincell blog app - built using reactjs and uses mui for css
+A simple token based logic was used for authentication
--- src/api/axios.js
+++ src/api/axios.js
@@ -4,12 +4,6 @@ const service = axios.create({
   timeout: 10000, // request timeout
 });
 
-// service.defaults.baseURL = 'http://localhost:8000/';
-//   service.defaults.baseURL ='https://zwc74bqvu4.execute-api.ap-south-1.amazonaws.com/dev1/'
-//   service.defaults.baseURL ="https://api-service.d0ljtrevpub3e.ap-south-1.cs.amazonlightsail.com/"
-// service.defaults.baseURL = 'https://brain-db.herokuapp.com/';
-// service.defaults.baseURL = process.env.REACT_APP_API_LOCAL;
 service.defaults.baseURL = process.env.REACT_APP_API_PROD;
-// service.defaults.headers.post['Content-Type'] = 'application/json';
 
 export default service;
